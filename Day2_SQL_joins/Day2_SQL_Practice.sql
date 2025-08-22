514c3286a8441c192486e0f449afc1a04891b4f6
PavaniKalathur25 <gunina1025@gmail.com>
2025-08-22 08:04:11 -0400

    Update Day2_SQL_Practice.sql

--- Day2_SQL_joins/Day2_SQL_Practice.sql
+++ Day2_SQL_joins/Day2_SQL_Practice.sql
@@ -1,1 +1,27 @@
 -- Day 2 SQL Practice
+
+-- INNER JOIN → get matching records from both tables
+SELECT c.customer_id, c.name, o.order_id, o.amount
+FROM customers c
+INNER JOIN orders o ON c.customer_id = o.customer_id;
+
+-- LEFT JOIN → get all customers even if no orders
+SELECT c.customer_id, c.name, o.order_id, o.amount
+FROM customers c
+LEFT JOIN orders o ON c.customer_id = o.customer_id;
+
+-- RIGHT JOIN → get all orders even if customer info missing
+SELECT c.customer_id, c.name, o.order_id, o.amount
+FROM customers c
+RIGHT JOIN orders o ON c.customer_id = o.customer_id;
+
+-- FULL OUTER JOIN → all records from both sides
+SELECT c.customer_id, c.name, o.order_id, o.amount
+FROM customers c
+FULL OUTER JOIN orders o ON c.customer_id = o.customer_id;
+
+-- SELF JOIN → find employees with the same manager
+SELECT e1.name AS employee, e2.name AS manager
+FROM employees e1
+JOIN employees e2 ON e1.manager_id = e2.employee_id;
+
